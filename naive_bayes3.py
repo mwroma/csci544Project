04952ff8cba4aec8f7a49f62b6858adb4702c72b
kaiyuanfan <kaiyuanfan2015@gmail.com>
2017-04-10 08:15:11 -0700

    add simple naive bayes

---
+++ naive_bayes3.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pickle
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
+with open('train_data.bat', 'rb') as fp:
+    data = pickle.load(fp)
+X = data['X']
+y = data['Y']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.23, random_state=592)
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+my_metrics = metrics.classification_report( y_test, predictions)
+print(my_metrics)
